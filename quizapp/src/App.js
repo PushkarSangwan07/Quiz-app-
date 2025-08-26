24857e7ea1d9ea75540551230781862453e6fa2b
PushkarSangwan07 <sangwanpushkar01@gmail.com>
2025-08-26 12:57:35 +0530

    Add files via upload

---
+++ quizapp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import RouterEg from './component/RouterEg';
+
+function App() {
+
+    return (
+
+        <RouterEg />
+
+    )
+}
+
+export default App;
---
+++ quizapp/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+// import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Auth0Provider 
+       domain="dev-jkbswfvqzon271d4.us.auth0.com"
+    clientId="wdhc59G4yvY4ZKt7Tt2sKfNMQV9E5Byw"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
+
+        <App />
+      </Auth0Provider>
+
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
